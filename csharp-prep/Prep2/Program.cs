e094fcac1266c6753a427e6df86356469f188234
Jordan Thurgood <jthurg@byui.edu>
2024-01-11 20:47:55 -0700

    Wrote code to calculate a user's lette grade.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,50 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        //Ask user for grade percentage.
+        Console.Write("Please enter your grade percentage: ");
+        string grade = Console.ReadLine();
+        int gradePercent = int.Parse(grade);
+
+        string letter = "";
+
+        //Determine grade letter for user's percentage.
+        if (gradePercent >= 90)
+        {
+            letter = "A";
+            // Console.WriteLine($"Your grade is {letter}.");
+        }
+        else if (gradePercent >= 80)
+        {
+            letter = "B";
+            // Console.WriteLine($"Your grade is {letter}.");
+        }
+        else if (gradePercent >= 70)
+        {
+            letter = "C";
+            // Console.WriteLine($"Your grade is {letter}.");
+        }
+        else if (gradePercent >= 60)
+        {
+            letter = "D";
+            // Console.WriteLine($"Your grade is {letter}.");
+        }
+        else
+        {
+            letter = "F";
+            // Console.WriteLine($"Your grade is {letter}.");
+        }
+
+        Console.WriteLine($"Your grade is: {letter}");
+
+        if (gradePercent >= 70)
+        {
+            Console.WriteLine("You passed.");
+
+        }
+        else
+        {
+            Console.WriteLine("You did not pass. Better luck next time!");
+        }
     }
 }
