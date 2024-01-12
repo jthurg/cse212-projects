68317564f96bf2a94317994899f4f998d490b1bf
Jordan Thurgood <jthurg@byui.edu>
2024-01-11 20:07:50 -0700

    Made it ask for and display name.

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,9 +1,23 @@
 using System;
+using System.ComponentModel.DataAnnotations;
+using System.Reflection;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        // Ask for first name.
+        Console.Write("What is your first name? ");
+        string first_name = Console.ReadLine();
+
+        // Ask for last name.
+        Console.Write("What is your last name? ");
+        string last_name = Console.ReadLine();
+
+        // Display name in special order.
+        Console.WriteLine();
+        Console.WriteLine($"Your name is {last_name}, {first_name} {last_name}.");
+
+        
     }
 }
