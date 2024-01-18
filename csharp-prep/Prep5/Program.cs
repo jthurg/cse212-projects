ace1b5e3afa31dca3efbc81efe2d7f9631d720fa
Jordan Thurgood <jthurg@byui.edu>
2024-01-18 12:39:56 -0700

    Added comments to Prep 5 code.

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,21 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
+        // Display the welcome message.
         DisplayWelcome();
+
+        //Retrieve the name of the user.
         string userName = PromptUserName();
+
+        // Retrieve the user's favorite number.
         int userNumber = PromptUserNumber();
+
+        //Square the user's favorite number.
         int squareNumber = SquareNumber(userNumber);
+
+        //Display the final results.
         DisplayResult(userName, squareNumber);
 
     }
 
     static void DisplayWelcome()
     {
+        //Display the welcome message.
         Console.WriteLine("Welcome to the Program!");
     }
 
     static string PromptUserName()
     {
+        //Prompt user for their name and return it as a string.
         Console.Write("Please enter your name: ");
         string name = Console.ReadLine();
         return name;
@@ -26,6 +37,7 @@ class Program
 
     static int PromptUserNumber()
     {
+        //Prompt user for favorite number and return it as an int.
         Console.Write("Please enter your favorite number: ");
         string userNumber = Console.ReadLine();
         int number = int.Parse(userNumber);
@@ -34,14 +46,20 @@ class Program
 
     static int SquareNumber(int number)
     {
+        //Square the user's number and return it as an int.
         int squareNumber = number * number;
         return squareNumber;
     }
 
     static void DisplayResult(string name, int squaredNumber)
     {
-        string numString = squaredNumber.ToString(); 
+        //Convert the user's number to a string.
+        string numString = squaredNumber.ToString();
+
+        //Create a string to display the user's name and the square number from their original number. 
         string message = name + ", the square of your number is " + numString + ".";
+
+        //Display the string created in the line above.
         Console.WriteLine(message);
     }
 }
