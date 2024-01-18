a58351e9eb1e8e456cfbbba714dac6a9fcdc9dab
Jordan Thurgood <jthurg@byui.edu>
2024-01-17 17:48:42 -0700

    Began Prep 5 code

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -6,4 +6,37 @@ class Program
     {
         Console.WriteLine("Hello Prep5 World!");
     }
+
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the Program!");
+    }
+
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+        return name;
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        string userNumber = Console.ReadLine();
+        int number = int.Parse(userNumber);
+        return number;
+    }
+
+    static int SquareNumber(int number)
+    {
+        int squareNumber = number * number;
+        return squareNumber;
+    }
+
+    static string DisplayResult(string name, int number)
+    {
+       string numString = 
+        string message = 
+    }
 }
+    
